b89d4f7a7adb8f0e020e7447c8c59953e0f25a1a
codydeatherage <cdeatherage14@gmail.com>
2022-06-17 03:42:20 -0700

    issues can now be uploaded

--- models/issue.js
+++ models/issue.js
@@ -1,12 +1,11 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
-const creatorSchema = new Schema({name: {type: String, required: true }, role:{type: String, required: true }})
 const Issue = new Schema(
     {
         title: { type: String, required: true },
         publisher:  { type: String, required: true },
         year:  { type: String, required: true },
-        creators:  [creatorSchema],
+        creators:  [{name: {type: String, required: true }, role:{type: String, required: true }}],
         coverImageURL:  { type: [String], required: true },
     },
     { timestamps: true },
